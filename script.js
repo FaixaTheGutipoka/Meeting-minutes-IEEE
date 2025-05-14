ad0a212c0f96940c8645946e70c0c9ff47aa754d
Mushtasin Ahmed <mushtasin.ahmed11@gmail.com>
2025-05-15 04:44:40 +0600

    Connecting the Pages

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const loginToggle=document.querySelector(".show-login");
 const loginForm=document.querySelector(".login-form");
 const signupForm=document.querySelector(".signup-form");
 
-const signUpBtn=document.querySelector(".sign-up-btn");
-const loginBtn=document.querySelector(".log-in-btn");
+const signUpBtn=document.querySelector(".form-singup");
+const loginBtn=document.querySelector(".form-login");
 
 signupToggle.addEventListener("click",(e)=>{
     e.preventDefault();
@@ -24,3 +24,18 @@ loginToggle.addEventListener("click",(e)=>{
     loginForm.classList.remove("inactive");
     signupForm.classList.add("inactive");
 })
+//Handling form submission 
+// document.getElementsByClassName(".form-login").addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     const formData=new FormData(this);
+//     fetch("PORT:8080",{
+//         method:"POST",
+//         body: formData
+//     }).then(res=>{
+//         if(res.ok){
+//             window.location.href="'./pages/home.html'"
+//         }else{
+//             alert("Sub failed");
+//         }
+//     })
+// })
